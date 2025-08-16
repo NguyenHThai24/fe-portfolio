578fcfc75fff3f3c3eb42ef17a03d8e0401bfc0d
Ngh Tahi <thaingh2402@gmail.com>
2025-08-16 15:50:50 +0700

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.svg";
-import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/me.png";
+// import HeaderSocials from "./HeaderSocials";
 import { BsRocketFill } from "react-icons/bs";
 
 const Header = () => {
@@ -10,14 +10,16 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Hoang Thai Nguyen</h1>
+        <h1 className="text-name">Hoang Thai Nguyen</h1>
         <h5 className="text-light">Frontend Developer</h5>
 
         <CTA />
         {/* <HeaderSocials /> */}
         <div className="me">
-          <img src={ME} alt="avatar me" />
+          <div className="me__bg-img"></div>
+          <img src={ME} alt="avatar me" className="me__avatar" />
         </div>
+
         <a href="#contact" className="scroll__down">
           Scroll Down
           <BsRocketFill className="scroll__down-icon" />
